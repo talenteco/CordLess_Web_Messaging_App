8a29fec60ba315a8722499378ff094c4eebd025f
talenteco <dazzle0099@gmail.com>
2023-02-22 19:43:19 +0530

    Started making the individualContacts frontend component

--- cordless/src/index.js
+++ cordless/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client';
+import individualContacts from './individualContacts';
+const element = new individualContacts("Utkarsh");
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render([1,2,3].map(function()
+{
+    return(element.individualContact());;
+}
+    ));
+console.log(element.state.contactName)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
